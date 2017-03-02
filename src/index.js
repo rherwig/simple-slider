8111787df82f931cabc1dfab0d1e8a30e4eed133
Rico Herwig <r.herwig@shopmacher.de>
2017-03-02 14:48:08 +0100

    removes init methods

--- src/index.js
+++ src/index.js
@@ -2,14 +2,4 @@ import './styles/main.scss';
 
 import Slider from './core/slider';
 
-async function init($slider, options = {}) {
-    return new Slider($slider, options);
-}
-
-async function initAll(options = {}) {
-    Array.from(document.querySelectorAll('.slider')).forEach(slider => {
-        new Slider(slider, options);
-    });
-}
-
 global.SimpleSlider = Slider;
